4501ba01eabb6fcec5b49bae0176f422079b4824
Will Bradley <wbbradley@github.com>
2020-06-28 22:50:40 -0600

    wip

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,43 @@
 module Main where
 
-import Lib
+import Lib (V2, Ray ( Ray ), rayAdd, origin, east)
+
+data JoinerKind = In | Out deriving Show
+data Piece = Straight { length :: Float,
+                        start :: JoinerKind,
+                        end :: JoinerKind }
+           | Curve { arcLength :: Float,
+                     radius :: Float,
+                     start :: JoinerKind,
+                     end :: JoinerKind }
+    deriving (Show)
+
+-- General assumption is that pieces default orientation is to the east = (1, 0).
+
+-- connectors :: Ray -> Piece -> [(Ray, JoinerKind)]
+connectors ray (Straight length start end) =
+  [(rayAdd ray (Ray origin pi), start),
+   (rayAdd ray (Ray east 0.0), end)]
+-- connectors _ _ = unknown
+
+straight = Straight 6.0 In Out
+joinerIn = Straight 2.0 In In
+joinerOut = Straight 2.0 Out Out
+
+arc radius count = Curve arcLength radius In Out
+  where
+    arcLength = (pi * 4.0 * radius) / count
+
+curveLong = arc 10.0 8
+curveShort = arc 7.5 8
+
+pieces :: [Piece]
+pieces = (replicate 10 straight)
+         ++ (replicate 20 curveLong)
+         ++ (replicate 10 curveShort)
+         ++ (replicate 2 joinerIn)
+         ++ (replicate 2 joinerOut)
 
 main :: IO ()
-main = someFunc
+main = do
+  print pieces
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,21 @@
 module Lib
-    ( someFunc
+    ( V2,
+      origin,
+      east,
+      Ray ( Ray ),
+      rayAdd
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+data V2 = V2 Float Float deriving (Show, Eq)
+origin = V2 0.0 0.0
+east = V2 1.0 0.0
+
+type Radians = Float
+data Ray = Ray { relPos :: V2, radians :: Radians }
+
+rayAdd :: Ray -> Ray -> Ray
+rayAdd (Ray (V2 x1 y1) θ) (Ray (V2 x2 y2) φ) =
+    (Ray (V2 (x1 + x2Rotated) (y1 + y2Rotated)) (θ + φ))
+  where
+   x2Rotated = x2 * cos(θ) - y2 * sin(θ)
+   y2Rotated = x2 * sin(θ) + y2 * cos(θ) 
